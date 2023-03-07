a050173aed7bf17170a2cb829cde6a8d0ada59f8
camila <camila.boscariol@ab-inbev.com>
2023-03-07 11:11:43 -0300

    adjacentElementsProduct

---
+++ adjacentElementsProduct/adjacentElementsProduct.js
@@ -0,0 +1,13 @@
+function solution(inputArray) {
+  let result = 0;
+  for (let i = 0; i < inputArray.length; i++) {
+    if (inputArray[i] * inputArray[i + 1] > result) {
+      result = inputArray[i] * inputArray[i + 1];
+    }
+  }
+  console.log(result);
+}
+
+const arr = [-23, 4, -3, 8, -12];
+
+solution(arr);
